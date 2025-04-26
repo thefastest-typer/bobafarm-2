83e101228f7f1b34f3929dbae4f2345f56c89054
thefastest-typer <ethan.sohacks@gmail.com>
2025-04-26 10:08:48 -0400

    Create bobafarm.js
    boba

---
+++ bobafarm.js
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Movie Reviews: Bundles and Bundles of Yarn</title>
+
+		<script>
+			function sayGreetings() {
+				var myName = document.getElementById("txt").value;
+				alert("Greetings, " + myName + "! welcome to the sigma forces");
+			}
+		</script>
+
+		<style>
+			
+			#myImage {
+				text-align: center;
+			}
+			body {
+				background-color: lightskyblue;
+			}
+
+			#titlePageTitle {
+				background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);	-webkit-background-clip: text;
+				color: transparent;
+				text-align: center;
+				border: 5px dashed gold;
+				font-family: arial;
+			}
+			#anHsixId {
+				text-align: center;
+				font-family: arial;
+			}
+			#aHeading {
+				font-family: Helvetica;
+			}
+			#afdcs {
+				color: red;
+				font-family: Helvetica;
+			}
+
+		</style>
+	</head>
+
+	<body>
+		<h1 id="titlePageTitle"> Movie Reviews: The Life of Tralala</h1>
+		<h6 id="anHsixId"> By Sigma</h6>
+
+		<input type="text" id="txt">
+		<input type="button" onclick="sayGreetings()" value="Click me!">
+
+		<p> <em>The Life of Tralala</em> is truly a masterpiece. The worldbuilding is something we've never experienced before. The character designs have not a single flaw. The introduction was so casual and friendly. The reality check at the middle was so soul-freezing. The plot twist near the end made me shed a tear. The suspense was killing me. The part where the main characters part forever was extremely emotional. The voice actors were perfect for their roles. The music just added to the atmosphere. All in all, <b> the best movie </b> I've watched in my entire life.</p>
+
+		 <a href="https://brainrot.fandom.com/wiki/Tralalero_Tralala">
+            <div id="tralaleroTralalaImage">
+              <img src="https://static.wikia.nocookie.net/brainrotnew/images/a/ac/Tralalero_tralala.jpg/revision/latest?cb=20250321131418" alt="tralalero tralala" width="400" height="550">
+            </div>
+            </a>
+
+			<form>
+			<h2 id="afdcs"> very cool</h2>
+			<p><em>hello</em></p>
+			<p> I consent to joining the sigma forces<input type="checkbox"> </p>
+			<p> check this box<input type="checkbox"> </p>
+			<input type="button" value="press me"
+			</form>
+		
+	</body>
+</html>
